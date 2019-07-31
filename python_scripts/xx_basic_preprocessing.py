d60104b0137795b57664a2a8221874f429f50789
Guillaume Lemaitre <g.lemaitre58@gmail.com>
2019-07-31 18:19:15 +0200

    iter

--- python_scripts/xx_basic_preprocessing.py
+++ python_scripts/xx_basic_preprocessing.py
@@ -180,21 +180,68 @@ print(
 # %% [markdown]
 # We can see that the training time and the number of iterations is much
 # shorter while the accuracy is equivalent.
+#
+# In the previous example, we split the original data into a training set and
+# a testing set. This strategy has several issues: in the setting where the
+# amount of data is limited, the subset of data used to train or test will be
+# small; and the splitting was done in a random manner and we have no
+# information regarding the confidence of the results obtained.
+#
+# Therefore, we can use what cross-validation. Cross-validation consists in
+# repeating this random splitting into training and testing sets and aggregate
+# the model performance. By repeating the experiment, one can get a the
+# fluctuation of the model performance.
+#
+# The function `cross_val_score` allows for such experimental protocol by
+# giving the model, the data and the target. Since there is several
+# cross-validation strategies, `cross_val_score` takes a parameter `cv` which
+# defines the splitting strategy.
 
-# %%
+# %% {"deletable": true, "editable": true}
 from sklearn.model_selection import cross_val_score
 
-score = cross_val_score(model, data_numeric, target)
-print(f"The R2 score is: {score.mean():.2f} +- {score.std():.2f}")
-print(score)
+score = cross_val_score(model, data_numeric, target, cv=5)
+print(f"The R2 score (mean +/- 1 std. dev.) is: "
+      f"{score.mean():.2f} +/- {score.std():.2f}")
+print(f"The different scores obtained are: \n{score}")
 
-# %%
+# %% [markdown]
+# Setting `cv=5` created 5 training and testing sets on which we trained and
+# tested a model. This strategy is called K-fold cross-validation where `K`
+# corresponds to the number of split.
+#
+# ## Work with categorical data
+#
+# In the previous section, we dealt with data for which numerical algorithms
+# are mathematically designed to work natively. However, real datasets contain
+# type of data which do not belong to this category and will require some
+# preprocessing. Such preprocessing will transform these data to be numerical
+# and thus natively handled by machine learning algorithms.
+#
+# Categorical data are broadly encountered in data science. Numerical data is a
+# continuous quantity corresponding to a real numbers while categorical data
+# are represented as discrete values. For instance, the variable `SEX` in our
+# previous dataset is a categorical variable because it encodes the data with
+# the two categories `male` and `female`.
+#
+# In the remainder of this section, we will present different strategies to
+# encode categorical data into numerical data which can be used by a
+# machine-learning algorithm.
+
+# %% {"deletable": true, "editable": true}
 categorical_columns = [
     'SOUTH', 'SEX', 'UNION', 'RACE', 'OCCUPATION', 'SECTOR', 'MARR'
 ]
 data_categorical = data[categorical_columns]
+print(data_categorical.head())
 
-# %%
+# %% [markdown]
+# ### Encode categories having an ordering
+#
+# The most intuitive strategy is to encode each category by a numerical value.
+# The `OrdinalEncoder` will transform the data in such manner.
+
+# %% {"deletable": true, "editable": true}
 from sklearn.preprocessing import OrdinalEncoder
 
 print(data_categorical.head())
@@ -205,7 +252,26 @@ data_encoded = encoder.fit_transform(data_categorical)
 print(f"The dataset encoded contains {data_encoded.shape[1]} features")
 print(data_encoded[:5])
 
-# %%
+# %% [markdown]
+# We can see that all categories have been encoded for each feature
+# independently. We can also notice that the number of feature before and after
+# the encoding is the same.
+#
+# However, one has to be careful when using this encoding strategy. The
+# encoding imposed an order regarding the categories: 0 is smaller than 1 which
+# is smaller than 2, etc. If the original categories did not have such order
+# then this encoding is not adequate and you should use one-hot encoding
+# instead.
+#
+# ### Encode categories which do not have an ordering
+#
+# As previously stated, `OrdinalEncoder` is encoding categorical data having
+# an ordering. In this case, the `OneHotEncoder` should be used. For a given
+# feature, it will create as many new columns as categories. For a sample,
+# the column corresponding to the category will be set to `1` while the other
+# columns will be set to `0`.
+
+# %% {"deletable": true, "editable": true}
 from sklearn.preprocessing import OneHotEncoder
 
 print(data_categorical.head())
@@ -216,30 +282,71 @@ data_encoded = encoder.fit_transform(data_categorical)
 print(f"The dataset encoded contains {data_encoded.shape[1]} features")
 print(data_encoded[:5])
 
-# %%
+# %% [markdown]
+# One can notice that the number of features after the encoding is larger than
+# in the original data.
+#
+# Once the encoding is done, we could integrate it inside a machine learning
+# pipeline as in the case with numerical data. In the following, we train a
+# linear classifier on the encoded data and check the performance of this
+# machine learning pipeline using cross-validation.
+
+# %% {"deletable": true, "editable": true}
 model = make_pipeline(OneHotEncoder(handle_unknown='ignore'), LinearSVR())
 score = cross_val_score(model, data_categorical, target)
-print(f"The R2 score is: {score.mean():.2f} +- {score.std():.2f}")
-print(score)
+print(f"The R2 score is: {score.mean():.2f} +/- {score.std():.2f}")
+print(f"The different scores obtained are: \n{score}")
 
+# %% [markdown]
+# ## Combining different preprocessing on different data type
+#
+# In the previous section, we saw that we need to treat data specifically
+# depending of their nature (i.e. numerical or categorical). We were capable
+# of making the preprocessing in two sequential steps but we did not present
+# any tool which could allow us to first preprocess the data depending of
+# their type and later on use these preprocessed data to train a single
+# machine learning model.
+#
+# Scikit-learn provides a `ColumnTransformer` which will dispatch some specific
+# columns to a specific transformer.
+#
+# We can first define the columns depending on their data type:
+# * binary encoding: it will corresponds to features for which categories can
+#   be encoded by the values 0 or 1.
+# * one-hot encoding: it will corresponds to features for which categories
+#   do not have a particular ordered and should be a column should be created
+#   for each category.
+# * scaling: it will corresponds to the numerical features which will be
+#   standardized.
 
-# %%
-from sklearn.compose import make_column_transformer
-from sklearn.linear_model import RidgeCV
-
+# %% {"deletable": true, "editable": true}
 binary_encoding_columns = ['MARR', 'SEX', 'SOUTH', 'UNION']
 one_hot_encoding_columns = ['OCCUPATION', 'SECTOR', 'RACE']
 scaling_columns = ['AGE', 'EDUCATION', 'EXPERIENCE']
 
-preprocessor = make_column_transformer(
-    (OrdinalEncoder(), binary_encoding_columns),
-    (OneHotEncoder(handle_unknown='ignore'), one_hot_encoding_columns),
-    (StandardScaler(), scaling_columns)
-)
+# %% [markdown]
+# We can now create our `ColumnTransfomer` by specifying a list of triplet
+# (preprocessor name, transformer, columns). Finally, we can merge this
+# "preprocessor" in a machine learning pipeline by adding a machine learning
+# model (e.g. a linear model) after the preprocessing.
+
+# %% {"deletable": true, "editable": true}
+from sklearn.compose import ColumnTransformer
+from sklearn.linear_model import RidgeCV
+
+preprocessor = ColumnTransformer([
+    ('binary-encoder', OrdinalEncoder(), binary_encoding_columns),
+    ('one-hot-encoder', OneHotEncoder(handle_unknown='ignore'),
+     one_hot_encoding_columns),
+    ('standard-scaler', StandardScaler(), scaling_columns)
+])
 model = make_pipeline(preprocessor, RidgeCV())
 score = cross_val_score(model, data, target)
 print(f"The R2 score is: {score.mean():.2f} +- {score.std():.2f}")
-print(score)
+print(f"The different scores obtained are: \n{score}")
 
 
-# %%
+# %% [markdown]
+# One can notice that the model, even more complex than in the previous
+# sections, follow the same API meaning that it `fit` is called to preprocess
+# the data and `score` is used to predict and check the model performance.
